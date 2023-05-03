794243a304d3fbeb2740948664790343129350b1
Andrew Van <vanandrew@wustl.edu>
2023-05-02 23:25:20 -0500

    :sparkles: add error message on error

--- NORDIC_main.m
+++ NORDIC_main.m
@@ -6,7 +6,8 @@ maxNumCompThreads(str2double(num_threads));
 ARG.noise_volume_last = str2double(num_noise);
 try
     NIFTI_NORDIC(mag, phase, base_out, ARG);
-catch
-    
+catch e
+    fprintf(1,'The identifier was:\n%s',e.identifier);
+    fprintf(1,'There was an error! The message was:\n%s',e.message);  
 end
 end
